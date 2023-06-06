dbfbcf84d45dbf9dfb0f8c1c4d7dc63af753b85e
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-06-06 21:28:05 +0200

    Sum of the squares of numbers.

--- SumOfSquareNumber/src/SumOfSquares.java
+++ SumOfSquareNumber/src/SumOfSquares.java
@@ -4,10 +4,10 @@ public class SumOfSquares {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Type a number to sum of squares: ");
-        int userNumber = scan.nextInt();
+        long userNumber = scan.nextInt();
         long sum = 0;
 
-        for (int i = 1; i <= userNumber ; i++) {
+        for (long i = 1; i <= userNumber ; i++) {
             sum += i * i;
         }
         System.out.println("Sum of the squares from 1 to "+ userNumber + ":" +
