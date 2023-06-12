7522f0bbb9dfb0e48b223381a98d39771283405b
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-06-12 11:18:51 +0200

    Strange polynomial sum

---
+++ polynomial/src/StrangePolynomialSum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StrangePolynomialSum {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type a number more than 0: ");
+
+        int userNumber = scan.nextInt();
+        int exponent = 1;
+        long sum = 0;
+
+        for (int i = 0; i < userNumber; i++) {
+            sum += Math.pow(i+1, exponent);
+
+            switch (exponent){
+                case 4-> exponent = 1;
+
+                default-> exponent++;
+            }
+        }
+
+        System.out.println("Result: "+sum);
+
+        scan.close();
+    }
+
+}
