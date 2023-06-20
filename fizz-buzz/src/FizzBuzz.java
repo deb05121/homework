ec78e7d5a87fa8d9e9e56cc8dc4fbe2035c159d1
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-06-20 15:49:38 +0200

    FizzBuzz has been fixed

--- fizz-buzz/src/FizzBuzz.java
+++ fizz-buzz/src/FizzBuzz.java
@@ -1,40 +1,28 @@
 import java.util.Scanner;
 
 public class FizzBuzz {
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Type a bigger number than 0:");
         int userNumber = scan.nextInt();
-        int n = 0;
-
-        if (userNumber < 1) {
+        while (userNumber < 1) {
             System.out.println("Please enter a number greater than 0");
-
+            userNumber = scan.nextInt();
         }
 
         for (int i = 1; i <= userNumber; i++) {
-            if (i % 3 == 0) {
-                n = 3;
-            }
-            if (i % 5 == 0) {
-                n = 5;
-            }
+            int n = 0;
             if (i % 3 == 0 && i % 5 == 0) {
-                n = 35;
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
-
-            switch (n) {
-                case 3 -> System.out.println("Fizz");
-                case 5 -> System.out.println("Buzz");
-                case 35 -> System.out.println("FizzBuzz");
-
-                default -> System.out.println(i);
-            }
-            n = 0;
         }
-
+        scan.close();
     }
-
 }
