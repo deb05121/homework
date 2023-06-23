3ae70eedc76dc4d9fe220eba978763cbca5679c8
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-06-23 22:32:07 +0200

    numberOfDivisor is initialized by 2

--- BreakWeek/src/HeavenNumbers.java
+++ BreakWeek/src/HeavenNumbers.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 
 public class HeavenNumbers {
     public static void main(String[] args) {
-        boolean heaven = false;
+        boolean isHeavenly = false;
         Scanner scan = new Scanner(System.in);
         System.out.println("Type a number: ");
         int userNumber = scan.nextInt();
 
         if (userNumber % 2 == 0 && userNumber < 100 && userNumber % 7 != 0) {
-            heaven = true;
+            isHeavenly = true;
         } else {
             if (userNumber > 300 && userNumber % 13 == 0) {
-                byte divisorPc = 0;
+                byte numberOfDivisor = 2;
                 int i = 2;
-                while (divisorPc < 9 && i <= (userNumber / 2)) {
+                while (numberOfDivisor < 9 && i <= (userNumber / 2)) {
 
                     if (userNumber % i == 0) {
                         //System.out.println(i);
-                        divisorPc++;
-                        if (divisorPc == 9) {
-                            heaven = true;
+                        numberOfDivisor++;
+                        if (numberOfDivisor == 9) {
+                            isHeavenly = true;
                         }
                     }
                     i++;
                 }
             }
         }
-        System.out.println("Is your number a heaven number? " + heaven);
+        System.out.println("Is your number a heaven number? " + isHeavenly);
         scan.close();
 
     }
